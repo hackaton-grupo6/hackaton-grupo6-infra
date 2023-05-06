7201487a0738a06ce64ef30b360d1805ae52831b
Ricardo Garcia <rngarcia@gmail.com>
2023-05-06 13:13:16 -0300

    criando storage e importando o arquivo sql

--- gcp-main.tf
+++ gcp-main.tf
@@ -6,6 +6,18 @@ resource "google_artifact_registry_repository" "my-repo" {
   format = "DOCKER"
 }
 
+# Criando o storage para o dump do sql
+resource "google_storage_bucket" "image-store" {
+  name     = "image-store-bucket"
+  location = "US"
+}
+
+resource "google_storage_default_object_acl" "image-store-default-acl" {
+  bucket = google_storage_bucket.image-store.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
 # Criando a instância Base de Dados mySQL
 resource "google_sql_database_instance" "instance" {
   name             = "sql-fiap-grupo06-prod"
@@ -14,6 +26,7 @@ resource "google_sql_database_instance" "instance" {
   settings {
     tier = "db-f1-micro"
   }
-
+  dump_file_path     = "gs://image-store-bucket/Playlist.sql"
+}
   deletion_protection  = "true"
 }
