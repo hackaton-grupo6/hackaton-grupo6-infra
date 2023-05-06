0933882de0a5c2743dc4d7dcc2f82bb08a5d54ec
Ricardo Garcia <rngarcia@gmail.com>
2023-05-06 12:20:26 -0300

    Criar DB MySQL

--- gcp-main.tf
+++ gcp-main.tf
@@ -4,3 +4,20 @@ resource "google_artifact_registry_repository" "my-repo" {
   description = "Imagens Docker"
   format = "DOCKER"
 }
+
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
+}
+
+# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
+resource "google_sql_database_instance" "instance" {
+  name             = "my-database-instance"
+  region           = "us-central1"
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
+}
